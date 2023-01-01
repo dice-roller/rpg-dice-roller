b735a99b9fca31d652fdec017121917653969e10
Lee Langley <lee@greenimp.co.uk>
2023-01-01 23:30:56 +0000

    Working on arbitrary dice

--- src/dice/ArbitraryDice.js
+++ src/dice/ArbitraryDice.js
@@ -1,5 +1,52 @@
-import StandardDice from './StandardDice.js';
+import {isNumeric} from '../utilities/math.js';
 
-class ArbitraryDice extends StandardDice {}
+const sidesSymbol = Symbol('sides');
+const qtySymbol = Symbol('qty');
+
+class ArbitraryDice {
+  /**
+   * Create an `ArbitraryDice` instance.
+   *
+   * @param {string|number|string[]|number[]} sides The sides of the die
+   * @param {number} [qty=1] The number of dice to roll (e.g. `4`)
+   */
+  constructor (sides, qty = 1) {
+    this.sides = sides;
+
+    if (!isNumeric(qty)) {
+      throw new TypeError('qty must be a positive finite integer');
+    } else if ((qty < 1) || (qty > 999)) {
+      throw new RangeError('qty must be between 1 and 999');
+    }
+
+    this[qtySymbol] = parseInt(`${qty}`, 10);
+  }
+
+  /**
+   * The number of sides the die has.
+   *
+   * @returns {number}
+   */
+  get sides() {
+    return this[sidesSymbol] || [];
+  }
+
+  /**
+   *
+   * @param {string|number|string[]|number[]} sides The sides of the die
+   */
+  set sides(sides) {
+    this[sidesSymbol] = Array.isArray(sides) ? sides : [sides];
+  }
+
+  /**
+   * The number of dice that should be rolled.
+   *
+   * @returns {number}
+   */
+  get qty() {
+    return this[qtySymbol];
+  }
+}
 
 export default ArbitraryDice;
