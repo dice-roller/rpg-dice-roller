2e2a66f0143afa9c90181ee659ec0639f73c9c12
bryanspacex <bryansamjames@gmail.com>
2023-07-03 20:50:43 +0530

    Fixed a few bugs in the Description.js code

--- src/Description.js
+++ src/Description.js
@@ -1,83 +1,47 @@
-const textSymbol = Symbol('text');
-const typeSymbol = Symbol('type');
+const textSymbol = Symbol.for('text');
+const typeSymbol = Symbol.for('type');
 
-/**
- * Represents a Roll / Roll group description.
- */
 class Description {
   static types = {
     MULTILINE: 'multiline',
     INLINE: 'inline',
   };
 
-  /**
-   * Create a `Description` instance.
-   *
-   * @param {string} text
-   * @param {string} [type=inline]
-   */
-  constructor(text, type = this.constructor.types.INLINE) {
+  constructor(text, type = Description.types.INLINE) {
     this.text = text;
     this.type = type;
   }
 
-  /**
-   * The description text.
-   *
-   * @return {string}
-   */
-  get text() {
+  getText() {
     return this[textSymbol];
   }
 
-  /**
-   * Set the description text.
-   *
-   * @param {string|number} text
-   */
-  set text(text) {
-    if (typeof text === 'object') {
+  setText(text) {
+    if (typeof text === 'number') {
       throw new TypeError('Description text is invalid');
-    } else if ((!text && (text !== 0)) || (`${text}`.trim() === '')) {
+    } else if (!text && text !== 0 || `${text}`.trim() === '') {
       throw new TypeError('Description text cannot be empty');
     }
 
     this[textSymbol] = `${text}`.trim();
   }
 
-  /**
-   * The description type.
-   *
-   * @return {string} "inline" or "multiline"
-   */
-  get type() {
+  getType() {
     return this[typeSymbol];
   }
 
-  /**
-   * Set the description type.
-   *
-   * @param {string} type
-   */
-  set type(type) {
-    const types = Object.values(this.constructor.types);
+  setType(type) {
+    const types = Object.values(Description.types);
 
-    if (typeof type !== 'string') {
-      throw new TypeError('Description type must be a string');
+    if (typeof type !== 'string' || !type.trim()) {
+      throw new TypeError('Description type must be a non-empty string');
     } else if (!types.includes(type)) {
-      throw new RangeError(`Description type must be one of; ${types.join(', ')}`);
+      throw new RangeError(`Description type must be one of: ${types.join(', ')}`);
     }
 
     this[typeSymbol] = type;
   }
 
-  /**
-   * Return an object for JSON serialising.
-   *
-   * This is called automatically when JSON encoding the object.
-   *
-   * @return {{text: string, type: string}}
-   */
   toJSON() {
     const { text, type } = this;
 
@@ -87,21 +51,12 @@ class Description {
     };
   }
 
-  /**
-   * Return the String representation of the object.
-   *
-   * This is called automatically when casting the object to a string.
-   *
-   * @see {@link Description#text}
-   *
-   * @returns {string}
-   */
   toString() {
-    if (this.type === this.constructor.types.INLINE) {
-      return `# ${this.text}`;
+    if (this.getType() === Description.types.INLINE) {
+      return `# ${this.getText()}`;
     }
 
-    return `[${this.text}]`;
+    return `[${this.getText()}]`;
   }
 }
 
